ab0105c2336b60f635159438ead0086b972a7169
Nadine Jamila <nadinejamila@gmail.com>
2015-04-09 15:39:13 +0800

    Adding chap 5 codes

---
+++ Exercises - Tango with Django/tango_with_django_project/rango/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(unique=True, max_length=128)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Page',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=128)),
+                ('url', models.URLField()),
+                ('views', models.IntegerField(default=0)),
+                ('category', models.ForeignKey(to='rango.Category')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
--- Exercises - Tango with Django/tango_with_django_project/rango/models.py
+++ Exercises - Tango with Django/tango_with_django_project/rango/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+
+    def __unicode__(self):  
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+        
+class Page(models.Model):
+    category = models.ForeignKey(Category)
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+
+    def __unicode__(self):
+        return self.title
