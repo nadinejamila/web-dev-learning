5916b15c6114a44104247cb51f150ec83d9e147c
Nadine Jamila <nadinejamila@gmail.com>
2015-04-09 19:41:55 +0800

    Exercises for Chap 7

--- Exercises - Tango with Django/tango_with_django_project/populate_rango.py
+++ Exercises - Tango with Django/tango_with_django_project/populate_rango.py
@@ -12,39 +12,47 @@ def populate():
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=100)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=70)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=40)
 
     django_cat = add_cat("Django", views=64, likes=32)
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=90)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=60)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=30)
 
     frame_cat = add_cat("Other Frameworks", views=32, likes=16)
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=80)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org", 
+        views=50)
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
--- Exercises - Tango with Django/tango_with_django_project/rango/views.py
+++ Exercises - Tango with Django/tango_with_django_project/rango/views.py
@@ -6,7 +6,9 @@ from rango.models import Category, Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'pages': page_list}
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
@@ -24,4 +26,5 @@ def category(request, category_name_slug):
         context_dict['category'] = category
     except Category.DoesNotExist:
         pass
+
     return render(request, 'rango/category.html', context_dict)
