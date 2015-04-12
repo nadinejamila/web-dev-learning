0ca8d74e750834a32c1e8a9fb563df9768a71d65
Nadine Jamila <nadinejamila@gmail.com>
2015-04-12 18:24:17 +0800

    Ensure clean urls (chap 8)

--- Exercises - Tango with Django/tango_with_django_project/rango/forms.py
+++ Exercises - Tango with Django/tango_with_django_project/rango/forms.py
@@ -24,6 +24,14 @@ class PageForm(forms.ModelForm):
 		model = Page
 		exclude = ('category',)
 
+	def clean(self):
+		cleaned_data = self.cleaned_data
+		url = cleaned_data.get('url')
+		if url and not url.startswith('http://'):
+			url = 'http://' + url
+			cleaned_data['url'] = url
+		return cleaned_data
+		
 
 class UserForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
