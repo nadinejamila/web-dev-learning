08d33d3e95e144fc63a48206a308d298a63d1b2f
Nadine Jamila <nadinejamila@gmail.com>
2015-04-12 13:00:19 +0800

    Chap 8 codes

--- Exercises - Tango with Django/tango_with_django_project/rango/urls.py
+++ Exercises - Tango with Django/tango_with_django_project/rango/urls.py
@@ -5,5 +5,6 @@ from rango import views
 urlpatterns = patterns('',
 	url(r'^$', views.index, name='index'),
 	url(r'^about/$', views.about, name='about'),
+	url(r'^add_category/$', views.add_category, name='add_category'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category')
 	)
--- Exercises - Tango with Django/tango_with_django_project/rango/views.py
+++ Exercises - Tango with Django/tango_with_django_project/rango/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from rango.models import Category, Page
+from rango.forms import CategoryForm
 
 
 def index(request):
@@ -11,13 +12,14 @@ def index(request):
                     'pages': page_list}
     return render(request, 'rango/index.html', context_dict)
 
+
 def about(request):
     context_dict = {'boldmessage': "I am me."}
     return render(request, 'rango/about.html', context_dict)
 
+
 def category(request, category_name_slug):
     context_dict = {}
-
     try:
         category = Category.objects.get(slug=category_name_slug)
         context_dict['category_name'] = category.name
@@ -26,5 +28,19 @@ def category(request, category_name_slug):
         context_dict['category'] = category
     except Category.DoesNotExist:
         pass
-
     return render(request, 'rango/category.html', context_dict)
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+
+        else:
+            print form.errors
+
+    else:
+        form = CategoryForm()
+    return render(request, 'rango/add_category.html', {'form': form})
