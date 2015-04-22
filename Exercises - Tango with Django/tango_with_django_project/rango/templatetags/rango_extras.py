d911aa3a6023fc500362761924bdbe777c010489
Nadine Jamila <nadinejamila@gmail.com>
2015-04-22 23:23:25 +0800

    Display page list with templatetag

--- Exercises - Tango with Django/tango_with_django_project/rango/templatetags/rango_extras.py
+++ Exercises - Tango with Django/tango_with_django_project/rango/templatetags/rango_extras.py
@@ -1,5 +1,5 @@
 from django import template
-from rango.models import Category
+from rango.models import Category, Page
 
 
 register = template.Library()
@@ -7,3 +7,11 @@ register = template.Library()
 @register.inclusion_tag('rango/cats.html')
 def get_category_list(cat=None):
 	return {'cats': Category.objects.all(), 'act_cat': cat}
+
+@register.inclusion_tag('rango/pages.html')
+def get_page_list(category=None):
+	if category:
+		pages = Page.objects.filter(category=category)
+	else:
+		pages = None
+	return {'pages': pages}
